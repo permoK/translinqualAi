77645fd67ef94260da63f54beb213008e8734dce
permoK <perminusk21@gmail.com>
2025-04-07 00:14:35 +0300

    rm Mzungumzo

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -70,7 +70,7 @@ export default function HomePage() {
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
-                  Mzungumzo AI Chat Platform
+                  Translinqual AI Chat Platform
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                   Experience the power of AI-driven conversations in Kenyan languages including Maasai, Kiswahili, Kikuyu and more
